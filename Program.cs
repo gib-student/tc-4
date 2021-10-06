bada3ca27cb1c987b9053958139b4f3bbd49d5e9
Keenan <lindseyk2@byui.edu>
2021-10-06 12:09:50 -0600

    This is the start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tc_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
